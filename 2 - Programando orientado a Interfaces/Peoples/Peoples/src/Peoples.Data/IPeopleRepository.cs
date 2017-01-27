a1d602c76b425ed69fc1d277a19bf5a0d2826940
German Kuber <german.kuber@studentpartner.com>
2017-01-27 19:39:43 -0300

    Programando orientado a Interfaces

---
+++ 2 - Programando orientado a Interfaces/Peoples/Peoples/src/Peoples.Data/IPeopleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Peoples.Data
+{
+    public interface IPeopleRepository
+    {
+        void AddPerson(Person newPerson);
+        void DeletePerson(string lastName);
+        List<Person> GetPeople();
+        Person GetPerson(string lastName);
+    }
+}
---
+++ 2 - Programando orientado a Interfaces/Peoples/Peoples/src/Peoples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Peoples.Data;
+
+namespace Peoples
+{
+    public class Program
+    {
+        static readonly IPeopleRepository PeopleRepo = new PeopleRepository();
+        public static void Main(string[] args)
+        {
+            //TODO : 02 - Recorro la lista de People desde una lista
+            //List<Person> peoples = PeopleRepo.GetPeople();
+
+            //foreach (var person in peoples)
+            //    WritePerson(person);
+
+            //TODO : 03 - Recorro la lista de People desde un IEnumerable
+            //IEnumerable<Person> peoples = PeopleRepo.GetPeople();
+            //foreach (var person in peoples)
+            //    WritePerson(person);
+
+            Console.Read();
+        }
+
+        private static void WritePerson(Person data)
+        {
+            Console.WriteLine($"First Name : {data.FirstName} , Last Name :  {data.LastName}, Date : {data.StartDate}");
+        }
+    }
+}
