59d9b3dd22fd029aa51a983ebd544c033874697b
German Kuber <german.kuber@studentpartner.com>
2017-01-30 12:01:09 -0300

    Se agrega Mock

--- 6 - Ioc/Peoples.Service.Test/PeopleServiceTest.cs
+++ 6 - Ioc/Peoples.Service.Test/PeopleServiceTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Moq;
 using Peoples.Repositories.Interface;
 using Xunit;
 
@@ -27,12 +29,12 @@ namespace Peoples.Service.Test
         [Fact]
         public void GetPeople_Se_agrega_2_raiting_a_anio_inpar()
         {
-            
+
             var mockRepository = new MockPeopleRepository();
             var newPerson = new Person
             {
                 LastName = "LastName Test",
-                StartDate = new DateTime(2011,1,1)
+                StartDate = new DateTime(2011, 1, 1)
             };
             IPeopleService service = new PeopleService(mockRepository);
             service.AddPerson(newPerson);
@@ -41,8 +43,35 @@ namespace Peoples.Service.Test
             var list = service.GetPeople();
             Assert.Equal(2, list.First().Rating);
         }
+        //TODO : 04 - Install-Package Moq -Pre
 
-      
+        [Fact]
+        public void GetPeople_Se_agrega_2_raiting_a_anio_inpar_con_mock()
+        {
+
+            //TODO : 04 - Implemento Mock para mockear mis servicios
+            var contextPeople = new List<Person>();
+            var newPerson = new Person
+            {
+                LastName = "LastName Test",
+                StartDate = new DateTime(2011, 1, 1)
+            };
+            Mock<IPeopleRepository> mockPeopleRepository = new Mock<IPeopleRepository>();
+            mockPeopleRepository.Setup(x => x.GetPeople()).Returns(contextPeople);
+            mockPeopleRepository.Setup(x => x.AddPerson(newPerson)).Callback(() =>
+            {
+                contextPeople.Add(newPerson);
+            });
+
+
+            IPeopleService service = new PeopleService(mockPeopleRepository.Object);
+            service.AddPerson(newPerson);
+
+
+            var list = service.GetPeople();
+            Assert.Equal(2, list.First().Rating);
+        }
+
     }
 
 
