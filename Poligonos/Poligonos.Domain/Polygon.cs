3170e099d57f34b93f670a585fabdec061282a42
German Kuber <german.kuber@studentpartner.com>
2017-02-23 10:24:24 -0300

    Nuevo proyecto de poligonos

---
+++ Poligonos/Poligonos.Domain/Polygon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Poligonos.Domain
+{
+    //TODO: 01 - Creo la clase Concreta RegularPolygon
+    public class RegularPolygon
+    {
+        public int Sides { get; set; }
+        public int SideLength { get; set; }
+
+        public RegularPolygon(int sides, int length)
+        {
+            Sides = sides;
+            SideLength = length;
+        }
+
+        public double GetPerimeter()
+        {
+            return Sides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Poligonos/Poligonos.Domain/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Poligonos.Domain
+{
+    //TODO: 08 - Impelmento la clase Triangle y heredo de la clase Abstracta  AbstractRegularPolygon
+    public class Triangle : AbstractRegularPolygon
+    {
+        public Triangle(int length) :
+            base(3, length)
+        { }
+        //TODO: 09 - Implemento el metodo GetArea
+        public override double GetArea()
+        {
+            return SideLength * SideLength * Math.Sqrt(3) / 4;
+        }
+    }
+}
